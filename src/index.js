799c76d25c68ddb70174b462ccf1ba3676d2e12e
Pimlada Kongkham <pimdiamond@gmail.com>
2021-08-05 23:12:04 -0400

    added index js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import { header } from './modules/header';
+import { footer } from './modules/footer';
+import { lunch } from './modules/lunchgenerator';
+
+
+class App {
+
+    constructor() {
+        this.renderTemplate();
+        // lunch.randomizeLunches();
+    }
+    
+    renderTemplate() {
+
+        const template = `
+      <h1>${header.title}</h1>
+      <h2>${header.subtitle}</h2>
+      <p>${lunch.menu}</p>
+      <footer>${footer.footer}</footer>
+`;
+
+
+document.body.innerHTML = template;
+    }
+
+}
+  
+new App;
